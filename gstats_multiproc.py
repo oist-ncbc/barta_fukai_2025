a4f28a95defa47dd5b397a349145e36c239a1468
Tom83B <tmbrta@gmail.com>
2025-02-10 14:42:45 +0900

    param update

--- gstats_multiproc.py
+++ gstats_multiproc.py
@@ -46,9 +46,20 @@ if __name__ == '__main__':
     #     config = yaml.safe_load(f)
     batch_size = 5
 
-    with h5py.File('data/hebb1000_cond_stats.h5', "w") as h5f:
-        h5f.create_group('exc')
-        h5f.create_group('inh')
+    # with h5py.File('data/hebb1000_cond_stats.h5', "w") as h5f:
+    #     h5f.create_group('exc')
+    #     h5f.create_group('inh')
+
+    stats = {
+        'mean_e': np.array([]),
+        'mean_i': np.array([]),
+        'std_e': np.array([]),
+        'std_i': np.array([]),
+        'cov': np.array([])
+    }
+
+    index_ei = []
+    index_n  = []
 
     for ei in ['exc','inh']:
         print(f'calculating {ei}')
@@ -57,8 +68,8 @@ if __name__ == '__main__':
             N = h5f['connectivity'].attrs[f'N_{ei}']
             print(f'{N} neurons')
             n_batches = N // batch_size
-            ge = h5f[f'state/{ei}/ge'][1000::1,:].T.reshape(n_batches, batch_size, -1)
-            gi = h5f[f'state/{ei}/gi'][1000::1,:].T.reshape(n_batches, batch_size, -1)
+            ge = h5f[f'state/{ei}/ge'][1000:,:].T.reshape(n_batches, batch_size, -1)
+            gi = h5f[f'state/{ei}/gi'][1000:,:].T.reshape(n_batches, batch_size, -1)
 
         with Pool(10) as pool:
             results = list(tqdm(pool.imap(process_batch, zip(ge, gi)), total=n_batches))
@@ -66,6 +77,18 @@ if __name__ == '__main__':
         means = np.concatenate(np.array([res[0] for res in results]))
         covs  = np.concatenate(np.array([res[1] for res in results]))
 
-        with h5py.File('data/hebb1000_cond_stats.h5', "a") as h5f:
-            h5f[ei].create_dataset('mean', data=means, dtype='float32')
-            h5f[ei].create_dataset('cov', data=covs, dtype='float32')
+        stats['mean_e'] = np.concatenate([stats['mean_e'], means[:,0]])
+        stats['mean_i'] = np.concatenate([stats['mean_i'], means[:,1]])
+        stats['std_e'] = np.concatenate([stats['std_e'], np.sqrt(covs[:,0,0])])
+        stats['std_i'] = np.concatenate([stats['std_i'], np.sqrt(covs[:,1,1])])
+        stats['cov'] = np.concatenate([stats['cov'], covs[:,0,1]])
+
+        index_ei.extend(N * [ei])
+        index_n.extend(list(range(N)))
+
+    df = pd.DataFrame(stats, index=[index_ei, index_n])
+    df['pearsonr'] = df['cov'] / (df['std_e'] * df['std_i'])
+    df[['mean_e','mean_i','std_e','std_i','pearsonr']].to_csv('data/hebb1000_cond_stats.csv')
+        # with h5py.File('data/hebb1000_cond_stats.h5', "a") as h5f:
+        #     h5f[ei].create_dataset('mean', data=means, dtype='float32')
+        #     h5f[ei].create_dataset('cov', data=covs, dtype='float32')
