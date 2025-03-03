d637b8019ea56c8d464260cc3755f6af1b7aa321
Tomas Barta <tomas-barta@ncbc01.oist.jp>
2025-03-03 11:43:31 +0900

    deigo scripts that assume training has been completed

---
+++ run_scripts/deigo_notrain.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+conditions=("hebb_recharge_plus" "hebb_recharge_minus" "hebb_recharge_plus_strong" "hebb_recharge_minus_strong")
+patterns=(1000 1200 1400 1600 1800 2000)
+run_id=$$
+simulation_id="$(date +%Y%m%d_%H%M%S)_$run_id"
+
+TMP_DIR="/tmp/simulation_$simulation_id"
+mkdir -p "$TMP_DIR"
+git archive --format=tar HEAD | tar -x -C "$TMP_DIR"
+cd "$TMP_DIR"
+
+for condition in "${conditions[@]}"; do
+    for num in "${patterns[@]}"; do
+        mkdir -p logs
+
+        ./run_scripts/run_notrain_deigo.sh "$num" "$condition" "$simulation_id" &
+    done
+done
